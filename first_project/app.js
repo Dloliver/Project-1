55d99642e6c12ba094f6f3cdb5a95e1757fe3c76
dennisloliver <doliver9110@gmail.com>
2021-10-05 10:19:16 -0400

    modal works pictures pop up in modal now

--- first_project/app.js
+++ first_project/app.js
@@ -5,10 +5,37 @@ $(() => {
     i = Math.floor(Math.random() * 671)
   });
 
+  const random = () => {
+    horoscope = Math.floor(Math.random() * (data.episode.length))
+  };
+
+  const $openBtn = $('#openModal');
+
+  // Grabbing modal element
+  const $modal = $('#modal');
+
+  // Grabbing close button
+  const $closeBtn = $('#close');
+
+  const openModal = () => {
+    $modal.css('display', 'block')
+  }
+
+  const closeModal = () => {
+    $modal.css('display', 'none')
+  }
+
+  $openBtn.on('click', openModal)
+  $closeBtn.on('click', closeModal)
+
 
 
   $("form").on("submit", (e) => {
     e.preventDefault();
+    $(".name").empty();
+    $('.photo').empty();
+    const $closeBtn = $('#close');
+    $closeBtn.on('click', closeModal)
 
     const userInput = $('input[type="text"]').val();
 
@@ -23,13 +50,21 @@ $(() => {
       //   $limit: userInput,
       // },
     }).then((data) => {
-      e.preventDefault();
-      const $newdiv = $("<div>").text(data.name).appendTo($(".name"));
+      e.preventDefault()
+      const $newdiv = $("<div>").text(data.name).addClass('.character-name').prependTo($(".name"));
       console.log(data);
       //$("#episode").text(data)
-
-      $(".modalcontent").attr("src", data.image);
+      const $newmodal = $("<div>").text(data.img).appendTo($newdiv);
+      const $newimg = $('<img>').appendTo($('.photo'));
+      $($newimg).attr('src', data.image);
       console.log(data.image);
+      const $readingName = $('<div>').text(data.name).addClass('.modal-name').prependTo($('.photo'))
+      const $episode = $('<li>').text(data.episode).appendTo($('.photo'))
+      //  const $episode = $('<div>').text(data.episode[horoscope]).appendTO($readingName);
+
+
+
+
     });
   });
 });
